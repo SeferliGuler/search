87c76d27639d3bd3ce4f27ddfe67e0b45c404883
SeferliGuler <seferliglr23@gmail.com>
2024-04-11 17:51:36 +0400

    search

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
     document.addEventListener("click", function(event) {
-        ul = document.getElementById("list-group");
+        let ul = document.getElementById("list-group");
+        let input = document.getElementById('searchInput')
         let valueesas =document.getElementById('searchInputesas')
         if (event.target.id === "check" && ul.getElementsByClassName("list-group-item").length <= 3 && valueesas.value !=='') {
             let li = `
         <li class="list-group-item">
-            <input type="text" id="searchInput" placeholder="${valueesas.value}">
+           <input type="text" id="searchInput" value="${valueesas.value}" readonly>
             <span id="clearSearch" class="clear-icon-li">&times;</span>
         </li>
         `
@@ -13,9 +14,19 @@
         ul.insertAdjacentHTML("beforeend",li);
 
         valueesas.value=''
-    
-    }else{
-        return;
+        }
+        
+    else if (event.target.id === "alt") {
+        let liArray = Array.from(ul.getElementsByClassName("list-group-item"));
+        liArray.sort((a, b) => {
+            let textA = a.querySelector("#searchInput").getAttribute("value")
+            let textB = b.querySelector("#searchInput").getAttribute("value")
+            if (textA < textB) return -1;
+            if (textA > textB) return 1;
+            
+        });
+        ul.innerHTML = ''; 
+        liArray.forEach(li => ul.appendChild(li)); 
     }
     })
 
@@ -26,7 +37,6 @@
         }
     });
 
-    
 
    document.addEventListener('click', function(event) {
     let valueesas =document.getElementById('searchInputesas')
